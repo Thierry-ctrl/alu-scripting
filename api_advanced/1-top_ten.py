bfef48bf8570d4b97501f4ec7ace919b9d8da0b2
Thierry-ctrl <156646802+Thierry-ctrl@users.noreply.github.com>
2025-04-07 21:47:39 +0300

    Update 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,17 +1,23 @@
 #!/usr/bin/python3
-"""Prints the title of the first 10 hot posts listed for a given subreddit"""
-
+"""Script that fetches the top 10 hot posts for a given subreddit."""
 import requests
 
 
 def top_ten(subreddit):
-    """Main function"""
-    URL = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    """Prints the top 10 hot post titles of a subreddit, or nothing if invalid."""
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
 
-    HEADERS = {"User-Agent": "PostmanRuntime/7.35.0"}
     try:
-        RESPONSE = requests.get(URL, headers=HEADERS, allow_redirects=False)
-        HOT_POSTS = RESPONSE.json().get("data").get("children")
-        [print(post.get('data').get('title')) for post in HOT_POSTS]
+        response = requests.get(url, headers=headers, allow_redirects=False)
+
+        # Check if valid subreddit
+        if response.status_code != 200:
+            return
+
+        posts = response.json().get('data', {}).get('children', [])
+        for post in posts:
+            print(post.get('data', {}).get('title'))
+
     except Exception:
-        print(None)
+        return
