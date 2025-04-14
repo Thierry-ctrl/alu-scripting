5cd566c3a7c02a932dda4c072a82ed24f154763b
Thierry-ctrl <156646802+Thierry-ctrl@users.noreply.github.com>
2025-04-14 13:35:45 +0200

    Update 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""Script that queries the Reddit API"""
+"""
+This module contains a function that queries the Reddit API
+and prints the titles of the first 10 hot posts for a given subreddit.
+"""
 
 import requests
 
