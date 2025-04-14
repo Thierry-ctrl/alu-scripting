c3b148a897d49ad68942b3207cf1aee62ced8d02
Thierry-ctrl <156646802+Thierry-ctrl@users.noreply.github.com>
2025-04-14 13:33:51 +0200

    Update 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,32 +1,26 @@
 #!/usr/bin/python3
+"""Script that queries the Reddit API"""
 
-"""
-Displays the titles of 10 hot posts listed for a subreddit
-"""
-
-from requests import get
-
+import requests
 
 def top_ten(subreddit):
-    """
-    The Function that fethces the Reddit API
-    """
+    """Queries the Reddit API and prints the titles of the first 10 hot posts for a given subreddit."""
     url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    headers = {"User-Agent": "python:subreddit.hot.posts:v1.0"}
-
+    headers = {"User-Agent": "Rthierry"}
+    
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
         
-        # Check if request was successful (status 200)
         if response.status_code == 200:
-            data = response.json().get("data", {}).get("children", [])
+            data = response.json()
+            posts = data["data"]["children"][:10]
             
-            if data:
-                for post in data:
-                    print(post.get("data", {}).get("title"))
-            else:
-                print("None")
+            for post in posts:
+                print(post["data"]["title"])
+        
         else:
             print("None")
-    except Exception:
+    
+    except requests.RequestException as error:
+        print(f"Error fetching data: {error}")
         print("None")
